80c849095abbcc7d140c38f7805b95ac4b0d2e09
Austin <90881695+Cinderblook@users.noreply.github.com>
2022-02-18 20:00:29 -0500

    update

--- Terraform/networking.tf
+++ Terraform/networking.tf
@@ -106,7 +106,7 @@ resource "azurerm_network_security_group" "linux1" {
   }
 }
 resource "azurerm_network_security_group" "winserv" {
-  name                = "Allow-RDP-SSH"
+  name                = "Allow-RDP-SSH-ANS"
   location            = azurerm_resource_group.east.location
   resource_group_name = azurerm_resource_group.east.name
   security_rule {
@@ -129,6 +129,17 @@ resource "azurerm_network_security_group" "winserv" {
     source_port_range          = "*"
     destination_port_range     = "22"
     source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+   security_rule {
+    name                       = "ANSIBLE"
+    priority                   = 103
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "5985"
+    source_address_prefix      = "${var.east_subnets}"
     destination_address_prefix = "*"
   }
 }
